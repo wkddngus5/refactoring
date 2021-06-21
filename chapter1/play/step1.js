4538cc0859b46c2ff9c228031a8a745737a39076
JangWoohyun <wkddngus5@naver.com>
2021-06-21 23:58:46 +0900

    feat: set functions position

--- chapter1/play/step1.js
+++ chapter1/play/step1.js
@@ -1,6 +1,5 @@
 const plays = require('./plays.json');
 const invoces = require('./invoces.json');
-const [invoice] = invoces;
 
 function statement(invoice, plays) {
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
@@ -12,67 +11,67 @@ function statement(invoice, plays) {
   result += `총액: ${usd(totalAmount())}\n`;
   result += `적립 포인트: ${totalVolumeCredits(invoice)}점\n`;
   return result;
-}
 
-console.log(statement(invoice, plays));
-
-function totalAmount() {
-  let result = 0;
-  for (let perf of invoice.performances) {
-    result += amountFor(perf);
+  function totalAmount() {
+    let result = 0;
+    for (let perf of invoice.performances) {
+      result += amountFor(perf);
+    }
+    return result;
   }
-  return result;
-}
-
-function totalVolumeCredits(invoice) {
-  let volumeCredits = 0;
-  for (let perf of invoice.performances) {
-    // 포인트를 적립한다.
-    volumeCredits += volumeCreditsFor(perf);
+  
+  function totalVolumeCredits(invoice) {
+    let volumeCredits = 0;
+    for (let perf of invoice.performances) {
+      // 포인트를 적립한다.
+      volumeCredits += volumeCreditsFor(perf);
+    }
+    return volumeCredits;
   }
-  return volumeCredits;
-}
-
-function usd(aNumber) {
-  return new Intl.NumberFormat(
-    'en-US',
-    { style: 'currency', currency: 'USD', minimumFractionDigits: 2 }
-  ).format(aNumber / 100);
-}
-
-function volumeCreditsFor(perf) {
-  let result = 0;
-  result += Math.max(perf.audience - 30, 0);
-  // 희극 관객 5명마다 추가 포인트를 제공한다
-  if ('comedy' === playFor(perf).type) {
-    result += Math.floor(perf.audience / 5);
+  
+  function usd(aNumber) {
+    return new Intl.NumberFormat(
+      'en-US',
+      { style: 'currency', currency: 'USD', minimumFractionDigits: 2 }
+    ).format(aNumber / 100);
   }
-  return result;
-}
-
-function playFor(perf) {
-  return plays[perf.playID];
-}
-
-function amountFor(perf) {
-  let result;
-  switch (playFor(perf).type) {
-    case 'tragedy': // 비극
-      result = 40000;
-      if (perf.audience > 30) {
-        result += 1000 * (perf.audience - 30);
-      }
-      break;
-    case 'comedy': // 희극 
-      result = 30000;
-      if (perf.audience > 20) {
-        result += 10000 + 500 * (perf.audience - 20);
-      }
-      result += 300 * perf.audience;
-      break;
-    default:
-      throw new Error(`알 수 없는 장르: ${playFor(perf).type}`);
+  
+  function volumeCreditsFor(perf) {
+    let result = 0;
+    result += Math.max(perf.audience - 30, 0);
+    // 희극 관객 5명마다 추가 포인트를 제공한다
+    if ('comedy' === playFor(perf).type) {
+      result += Math.floor(perf.audience / 5);
+    }
+    return result;
   }
-  return result;
+  
+  function playFor(perf) {
+    return plays[perf.playID];
+  }
+  
+  function amountFor(perf) {
+    let result;
+    switch (playFor(perf).type) {
+      case 'tragedy': // 비극
+        result = 40000;
+        if (perf.audience > 30) {
+          result += 1000 * (perf.audience - 30);
+        }
+        break;
+      case 'comedy': // 희극 
+        result = 30000;
+        if (perf.audience > 20) {
+          result += 10000 + 500 * (perf.audience - 20);
+        }
+        result += 300 * perf.audience;
+        break;
+      default:
+        throw new Error(`알 수 없는 장르: ${playFor(perf).type}`);
+    }
+    return result;
+  }  
 }
+
+console.log(statement(invoces[0], plays));
 
